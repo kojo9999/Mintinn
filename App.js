f00f755a04bb4dc0b106f84d4b216481b533aa0a
D00215674 <d00215674@student.dkit.ie>
2021-01-20 17:17:55 +0000

    Built system for user TextInput to be  svaed then stored in Firestore, currently creates new Doc each time, must update for better functionality

--- App.js
+++ App.js
@@ -17,11 +17,11 @@ import {Ionicons, MaterialCommunityIcons, FontAwesome} from '@expo/vector-icons'
 class App extends React.Component{
     constructor(){
         super();
-        this.initializeFirebase();
+        //this.initializeFirebase();
     }
-    initializeFirebase = () => {
-        !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : null  
-    };
+    //initializeFirebase = () => {
+        //!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : null  
+    //};
     render(){
     return <AppContainer/>;
 }
--- screens/SleepScreen.js
+++ screens/SleepScreen.js
@@ -1,21 +1,75 @@
-import React, {Component} from "react"
-import {View, Text, StyleSheet} from "react-native"
+import React, {Component, useState} from "react";
+import {View, Text, StyleSheet, TextInput, Button} from "react-native";
+import 'firebase/firestore';
+import * as firebase from 'firebase';
+import {Alert} from "react-native";
+import {db} from '../config/config'
 
-class SleepScreen extends Component {
+export default class SleepScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+                createdAt: "",
+                sleep: "",
+                sleepInput: ""
+        }
+        db.collection('test').get().then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+                console.log(doc.data())
+                this.setState({
+                        createdAt: doc.data().createdAt,
+                        sleep: doc.data().sleep
+                })
+            })
+        })
+    }
+
+    /*async getSleep() {
+        const sleepInfo = [];
+        await db.collection('test').get().then(snapshot => {
+            snapshot.docs.forEach(doc => {
+                sleepInfo.push({createdAt: doc.createdAt, sleep: doc.sleep})
+            })
+        })
+        console.log(sleepInfo)
+        return sleepInfo;
+    }*/
+
+    saveSleep = () => {
+        db.collection('test').add({
+            createdAt: new Date(),
+            sleep: this.state.sleep
+        })
+    }
     render() {
+        let input = '';
+        const saveSleepInput = sleepInput => {
+            const input = sleepInput;
+            return input;
+        };
+
         return (
-            <View style={StyleSheet.container}>
-                <Text>SleepScreen</Text>
+            <View style={styles.container}>
+                <Text>Enter Input</Text>
+                <TextInput style={styles.input} placeholder='e.g. 8' onChangeText={sleepInput => this.setState({sleep: sleepInput})}/>
+                <Button title='Save to Firestore' onPress={() => this.saveSleep()}></Button>
+                <Text>Hours of Sleep: {this.state.sleep} </Text>
             </View>
-        );
+        )
     }
 }
-export default SleepScreen;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#777',
+        padding: 8,
+        margin: 10,
+        width: 200,
     }
 });
