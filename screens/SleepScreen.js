6a63ead39e6b91a29f90a07c1ce53db6cdfb211c
D00215674 <d00215674@student.dkit.ie>
2021-01-29 17:21:45 +0000

    system now creates new doc for each day, updates when necessary

--- screens/SleepScreen.js
+++ screens/SleepScreen.js
@@ -4,6 +4,7 @@ import 'firebase/firestore';
 import * as firebase from 'firebase';
 import {Alert} from "react-native";
 import {db} from '../config/config'
+const sleepCollection = db().collection('profile');
 
 export default class SleepScreen extends React.Component {
     constructor(props) {
@@ -13,32 +14,41 @@ export default class SleepScreen extends React.Component {
                 sleep: "",
                 sleepInput: ""
         }
-        db.collection('test').get().then((snapshot) => {
-            snapshot.docs.forEach(doc => {
-                console.log(doc.data())
-                this.setState({
-                        createdAt: doc.data().createdAt,
-                        sleep: doc.data().sleep
-                })
-            })
-        })
     }
 
-    /*async getSleep() {
-        const sleepInfo = [];
-        await db.collection('test').get().then(snapshot => {
-            snapshot.docs.forEach(doc => {
-                sleepInfo.push({createdAt: doc.createdAt, sleep: doc.sleep})
-            })
-        })
-        console.log(sleepInfo)
-        return sleepInfo;
-    }*/
+    HandleGetUserId=()=>{
+        let userId = firebase.auth().currentUser.uid;
+        return userId;
+      }
 
     saveSleep = () => {
-        db.collection('test').add({
-            createdAt: new Date(),
-            sleep: this.state.sleep
+        let today = new Date();
+        let batch = firebase.firestore().batch();
+        let userId = this.HandleGetUserId();
+        sleepCollection.doc(userId).collection('sleep')
+        .where('createdat', '>', new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0))
+        .where('createdat', '<', new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59))
+        .get().then((snapshot) => {
+            console.log('Sleep Doc instances retrieved:', snapshot.size)
+            if(snapshot.size == 0) {
+                sleepCollection.doc(userId).collection('sleep').add({
+                    createdat: new Date(),
+                    sleepamount: this.state.sleep
+                })
+            }
+            else if(snapshot.size == 1) {
+                snapshot.docs.forEach((doc) => {
+                const newSleepDoc = {
+                    createdat: new Date(),
+                    sleepamount: this.state.sleep
+                };
+                const docRef = sleepCollection.doc(userId).collection('sleep').doc(doc.id)
+                batch.update(docRef, newSleepDoc)
+                batch.commit().then(() => {
+                    console.log('Sleep document was found and has been updated')
+                  })
+                })
+            }
         })
     }
     render() {
