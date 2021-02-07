2ca677f510fec2b26454408f0b755a4176c1b4c5
D00212522 <peterlacey7676@gmail.com@>
2021-02-07 19:37:43 +0000

    quick hotfix to do with getsleepprogress

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -31,53 +31,6 @@ export default class CalendarScreen extends React.Component {
                         createdat: doc.data().createdat,
                         waterstatus: doc.data().waterstatus,
                     }
-<<<<<<< HEAD
-                    waterdata.push(newWaterData);
-                    console.log('returned water value',waterdata) 
-                })
-                this.setState({water: waterdata})
-                    console.log('returned water value',this.state.water) 
-            })
-        }
-
-        // getFoodProgress = async () => {
-        //     const today = new Date();
-        //     let userId = this.HandleGetUserId();
-        //     waterCollection.doc(userId).collection('food')
-        //         .where("createdat", ">", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0))
-        //         .where("createdat", "<", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59))
-        //         .get().then((snapshot) => {
-        //             snapshot.docs.forEach((doc) => {
-        //                 const newFoodData={
-        //                     createdat: doc.data().createdat,
-        //                     foodstatus: doc.data().foodstatus,
-        //                 }
-        //                 this.setState({food: this.state.food.push(newFoodData)}) 
-        //                 console.log('returned newFoodData data', newFoodData) 
-        //                 console.log('returned food value',this.state.food)
-        //             })
-        //         })
-        //     }
-
-        //     getSleepProgress = async () => {
-        //         const today = new Date();
-        //         let userId = this.HandleGetUserId();
-        //         waterCollection.doc(userId).collection('sleep')
-        //             .where("createdat", ">", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0))
-        //             .where("createdat", "<", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59))
-        //             .get().then((snapshot) => {
-        //                 snapshot.docs.forEach((doc) => {
-        //                     const newSleepData={
-        //                         createdat: doc.data().createdat,
-        //                         sleepstatus: doc.data().sleepstatus,
-        //                     }
-        //                     this.setState({sleep: this.state.sleep.push(newSleepData)})
-        //                     console.log('returned newSleepData data', newSleepData)
-        //                     console.log('returned sleep value', this.state.sleep)
-        //                 })
-        //             })
-        //         }    
-=======
                     waterData.push(newWaterData); 
                 })
                 this.setState({water: waterData});
@@ -118,13 +71,12 @@ export default class CalendarScreen extends React.Component {
                                 createdat: doc.data().createdat,
                                 sleepamount: doc.data().sleepamount,
                             }
-                            sleepData.push(doc.data());
+                            sleepData.push(newSleepData);
                         })
                         this.setState({sleep: sleepData});
                         console.log("check for returned Sleep value",this.state.sleep);
                     })
                 }
->>>>>>> d8bdbd0eaff96fa5ed651f691f6723e6370c6c1c
 
     HandleGetUserId = () => {
         let userId = firebase .auth().currentUser.uid;
@@ -132,17 +84,10 @@ export default class CalendarScreen extends React.Component {
     };
 
     componentDidMount() {
-<<<<<<< HEAD
-    this.getWaterProgress()
-    
-    // this.getFoodProgress()
-    // this.getSleepProgress()
-=======
         this.getWaterProgress()
         this.getSleepProgress()
         this.getFoodProgress()
         console.log("Check Sleep State after call:", this.state.sleep)
->>>>>>> d8bdbd0eaff96fa5ed651f691f6723e6370c6c1c
     }
 
     render() {
@@ -151,11 +96,6 @@ export default class CalendarScreen extends React.Component {
                 <Text>Water Progress</Text>
                 <Progress.Bar progress={0.17} width={200} />
                 {/* <TouchableOpacity style={styles.button} onPress={() => this.CheckTodaysWater()}><Text style={styles.Text}>Test</Text></TouchableOpacity> */}
-<<<<<<< HEAD
-                {/* {this.state.water.map((data,index) => {
-            return(<Text style={styles.Text} key={index}>{data}</Text>)
-            })} */}
-=======
                 <Text>Sleep Array Data</Text>
             {this.state.sleep.map((data, index) => {
                 return(<Text style={styles.text} key={index}>{data.sleepamount}</Text>)
@@ -168,7 +108,6 @@ export default class CalendarScreen extends React.Component {
             {this.state.food.map((data, index) => {
                 return(<Text style={styles.text} key={index}>{data.foodamount}</Text>)
             })}
->>>>>>> d8bdbd0eaff96fa5ed651f691f6723e6370c6c1c
             </View>
         );
     }
