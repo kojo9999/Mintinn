36562f5814bd4b07731bba320f55bef40b0ae13b
D00212522 <peterlacey7676@gmail.com@>
2021-02-08 21:06:14 +0000

    Added the sign-out button and basic validation to sleep screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,11 +1,30 @@
-import React, {Component} from "react"
-import {View, Text, StyleSheet} from "react-native"
+import React from "react";
+import { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 class SettingsScreen extends Component {
+    signOut = async () => {
+        try {
+            await firebase.auth().signOut();
+            this.props.navigation.navigate("WelcomeScreen");
+        } catch (errors) {
+            alert("unable to sign out right now");
+        }
+    };
     render() {
         return (
-            <View style={StyleSheet.container}>
-                <Text>SettingsScreen</Text>
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => this.signOut()}>
+                    <Text style={styles.signout}>Sign Out</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -17,5 +36,16 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
-    }
+    },
+    button: {
+        height: 50,
+        width: 200,
+        borderRadius: 30,
+        backgroundColor: "black",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    signout: {
+        color: "white",
+    },
 });
