17e77b26c484f9ce4f5d14ba9bf7a5569ef92826
Erykj1072 <erykjagla@protonmail.com>
2021-02-15 22:05:51 +0000

    add buttons, add new graph module with mock data

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -8,71 +8,104 @@ import {
   LineChart
 } from "react-native-chart-kit";
 export default class CalendarScreen extends Component {
-  constructor() {
-    super();
-    this.state = {
-        foodData: [],
-        foodDate: [],
-        waterData: [],
-        waterDate: [],
-        sleep: []
-    }
+
+constructor() {
+  super();
+  this.state = {
+    foodData: [],
+    weeklyfoodData: [],
+    foodDate: [],
+    waterData: [],
+    weeklywaterData: [],
+    waterDate: [],
+    sleep: []
+  }
 }
 
 getFoodProgress = async () => {
   const today = new Date();
   let userId = this.HandleGetUserId();
+  let weekago = (new Date(new Date() - (86400000 * 6)))
   waterCollection.doc(userId).collection('food')
-      .where("createdat", ">", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0))
-      .where("createdat", "<", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59))
-        .get().then((snapshot) => {
-          snapshot.docs.forEach((doc) => {
-            this.state.foodData.push(doc.data().foodamount);
-            this.state.foodDate.push(doc.data().createdat);
-            console.log(doc.data().foodamount)
-          })
+    .where("createdat", ">", new Date(weekago.getFullYear(), weekago.getMonth(), weekago.getDate(), 0, 0, 0))
+    .where("createdat", "<", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59))
+    .get().then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        this.state.foodData.push(doc.data().foodamount);
+        this.state.foodDate.push(doc.data().createdat);
+        // console.log(doc.data().foodamount)
       })
-  }
+    })
+}
 
 getWaterProgress = async () => {
-    const today = new Date();
-    let userId = this.HandleGetUserId();
-    waterCollection.doc(userId).collection('water')
-        .where("createdat", ">", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0))
-        .where("createdat", "<", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59))
-        .get().then((snapshot) => {
-          snapshot.docs.forEach((doc) => {
-            this.state.waterData.push(doc.data().waterstatus);
-            this.state.waterDate.push(doc.data().createdat);
-            console.log(doc.data().waterstatus)
-          })
-        })
-    }
+  const today = new Date();
+  let userId = this.HandleGetUserId();
+  let weekago = (new Date(new Date() - (86400000 * 6)))
+  waterCollection.doc(userId).collection('water')
+    .where("createdat", ">", new Date(weekago.getFullYear(), weekago.getMonth(), weekago.getDate(), 0, 0, 0))
+    .where("createdat", "<", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59))
+    .get().then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        this.state.waterData.push(doc.data().waterstatus);
+        this.state.waterDate.push(doc.data().createdat);
+        // console.log(doc.data().waterstatus)
+      })
+    })
+}
 
-        getSleepProgress = async () => {
-            const today = new Date();
-            let userId = this.HandleGetUserId();
-            var sleepData = this.state.sleep;
-            waterCollection.doc(userId).collection('sleep')
-                .where("createdat", ">", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0))
-                .where("createdat", "<", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59))
-                .get().then((snapshot) => {
-                    snapshot.docs.forEach((doc) => {
-                        const newSleepData={
-                            createdat: doc.data().createdat,
-                            sleepamount: doc.data().sleepamount,
-                        }
-                        sleepData.push(newSleepData);
-                    })
-                    this.setState({sleep: sleepData});
-                    // console.log("check for returned Sleep value",this.state.sleep);
-                })
-            }
+getSleepProgress = async () => {
+  const today = new Date();
+  let userId = this.HandleGetUserId();
+  var sleepData = this.state.sleep;
+  waterCollection.doc(userId).collection('sleep')
+    .where("createdat", ">", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0))
+    .where("createdat", "<", new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59))
+    .get().then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        const newSleepData = {
+          createdat: doc.data().createdat,
+          sleepamount: doc.data().sleepamount,
+        }
+        sleepData.push(newSleepData);
+      })
+      this.setState({ sleep: sleepData });
+      // console.log("check for returned Sleep value",this.state.sleep);
+    })
+}
+
+getAverageProgress = async () => {
+  let data = this.state.foodData;
+  let newdata = []
+  // const empty = newdata => newdata.length = 0;
+  // empty(newdata)
+  let day7 = Number(((data[0] + data[1] + data[2]) / 3).toFixed());
+  let day6 = Number(((data[4] + data[4] + data[5]) / 3).toFixed());
+  let day5 = Number(((data[6] + data[7] + data[8]) / 3).toFixed());
+  let day4 = Number(((data[9] + data[10] + data[11]) / 3).toFixed());
+  let day3 = Number(((data[12] + data[13] + data[14]) / 3).toFixed());
+  let day2 = Number(((data[15] + data[16] + data[17]) / 3).toFixed());
+  let day1 = Number(((data[18] + data[19] + data[20]) / 3).toFixed());
+  newdata.push(day7, day6, day5, day4, day3, day2, day1);
+  console.log(newdata)
+  return newdata
+};
 
 HandleGetUserId = () => {
     let userId = firebase .auth().currentUser.uid;
     return userId;
 };
+
+refresh = () =>
+  { 
+    let refreshFoodData = (this.state.foodData)
+    let refreshWaterData = (this.state.waterData)
+    const empty = refresh => refresh.length = 0;
+    empty(refreshFoodData)
+    empty(refreshWaterData)
+    this.componentDidMount()
+  }
+
 
 async componentDidMount () {
     await  this.getWaterProgress()
