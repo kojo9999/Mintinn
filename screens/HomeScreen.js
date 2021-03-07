0da82f43d6c230f39cebd0a68f0008506ab33989
Erykj1072 <erykjagla@protonmail.com>
2021-03-07 10:05:09 +0000

    fix dashboard top cut off on smaller screens

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -128,6 +128,7 @@ const styles = StyleSheet.create({
     marginLeft: 20,
   },
   headerView: {
+    marginTop: 20,
     alignSelf: "stretch",
     flexDirection: "row",
     justifyContent: "center",
